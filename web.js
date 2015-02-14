b9fc37542443fa44d8dad271ba795c5377cc8144
Sho <soboy2@users.noreply.github.com>
2015-02-13 23:59:23 -0500

    Testing FileReadSync

--- web.js
+++ web.js
@@ -10,8 +10,10 @@ var accessLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a
 
 app.use(morgan('combined', {stream: accessLogStream}));
 
+var dataBuffer = fs.readFileSync('index.html');
+var dataString = dataBuffer.toString('utf8');
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(dataString);
 });
 
 var port = process.env.PORT || 5000;
