8bf4865c592ee150f223cbfd9af050141a0fec85
Sho <soboy2@users.noreply.github.com>
2015-02-16 16:49:37 -0500

    Added abilit for grader.js to test against urls

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 
 
 var fs = require('fs');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
@@ -38,17 +39,12 @@ var assertFileExists = function(infile) {
   return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-  return cheerio.load(fs.readFileSync(htmlfile));
-};
-
 var loadChecks = function(checksfile) {
-  return JSON.parse(fs.readFileSync(checksfile));
+  return JSON.parse(fs.readFileSync(checksfile)).sort();
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-  $ = cheerioHtmlFile(htmlfile);
-  var checks = loadChecks(checksfile).sort();
+var checkHtml = function(html, checks) {
+  $ = cheerio.load(html);
   var out = {};
   for(var ii in checks) {
     var present = $(checks[ii]).length > 0;
@@ -56,6 +52,28 @@ var checkHtmlFile = function(htmlfile, checksfile) {
   }
   return out;
 };
+
+var checkHtmlFile = function(htmlfile, checks) {
+  return checkHtml(fs.readFileSync(htmlfile), checks);
+};
+
+// var cheerioHtmlFile = function(htmlfile) {
+//   return cheerio.load(fs.readFileSync(htmlfile));
+// };
+
+var download = function(url, callback) {
+  var resp = rest.get(url);
+  resp.on('complete', function(result) {
+    if(result instanceof Error) {
+      sys.puts('Error: ' + result.message);
+      this.retry(5000);
+      return;
+    }
+    callback(null, result);
+  });
+};
+
+
 
 var clone = function(fn) {
   // http://stackoverflow.com/a/6772648
@@ -68,10 +86,28 @@ if(require.main == module) {
       clone(assertFileExists), CHECKSFILE_DEFAULT)
     .option('-f, --file <html_file>', 'Path to index.html',
       clone(assertFileExists), HTMLFILE_DEFAULT)
+    .option('-u, --url <url>', 'Path to download url')
     .parse(process.argv);
-  var checkJson = checkHtmlFile(program.file, program.checks);
-  var outJson= JSON.stringify(checkJson, null, 4);
-  console.log(outJson);
+
+  var check = function(err, html) {
+      if(err) {
+        console.log('Error getting html: ' + err);
+        process.exit(1);
+      }
+
+      var checks = loadChecks(program.checks);
+      var checkJson = checkHtml(html, checks);
+      var outJson = JSON.stringify(checkJson, null, 4);
+      console.log(outJson);
+  };
+
+  if(program.url) {
+    download(program.url, check);
+  } else if(program.file) {
+    fs.readFile(program.file, check);
+  }
+
 } else {
+  exports.loadChecks = loadChecks;
   exports.checkHtmlFile = checkHtmlFile;
 }
